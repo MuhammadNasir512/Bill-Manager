994f797a38ab2fdf9921d212176a0287edb6dad3
Muhammad Nasir <muhammadnasir512@yahoo.com>
2014-07-27 14:05:47 +0100

    Initial Commit

---
+++ BillsManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BillsManager
+//
+//  Created by Muhammad Nasir on 27/07/2014.
+//  Copyright (c) 2014 Muhammad Nasir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
